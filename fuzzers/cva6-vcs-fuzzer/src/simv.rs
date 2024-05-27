55862e549fccab674f2c21f2ff0077cf740a0f63
Corteggiani, Nassim <nassim.corteggiani@intel.com>
2024-05-26 22:44:38 -0700

    silent simv

--- fuzzers/cva6-vcs-fuzzer/src/simv.rs
+++ fuzzers/cva6-vcs-fuzzer/src/simv.rs
@@ -158,14 +158,13 @@ impl<'a> CommandConfigurator for SimvCommandConfigurator<'a> {
         
             let mut child = Command::new("bash")
              .arg("./run.sh")
-             .stdin(Stdio::piped())
-             .stdout(Stdio::piped())
-             .stderr(Stdio::piped())
+             .stdin(Stdio::null())
+             .stdout(Stdio::null())
+             .stderr(Stdio::null())
              .spawn()
              .unwrap();
 
             let secs = Duration::from_secs(40);
-            let secs = Duration::from_secs(350);
             
             let _status_code = match child.wait_timeout(secs).unwrap() {
                 Some(status) => status.code(),
@@ -175,12 +174,12 @@ impl<'a> CommandConfigurator for SimvCommandConfigurator<'a> {
                 }
             };
 
-            let mut s = String::new();
-            child.stdout.unwrap().read_to_string(&mut s).unwrap();
+            //let mut s = String::new();
+            //child.stdout.unwrap().read_to_string(&mut s).unwrap();
 
-            for (num, line) in s.split("\n").enumerate() {
-                println!("{}: {}", num, line);
-            }
+            //for (num, line) in s.split("\n").enumerate() {
+            //    println!("{}: {}", num, line);
+            //}
 
             Ok(Command::new("ls")
             .stdin(Stdio::piped())
