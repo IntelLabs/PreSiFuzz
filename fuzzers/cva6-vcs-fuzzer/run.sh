10992c21b385adcccd39caae52d851b6a3fac67c
Corteggiani, Nassim <nassim.corteggiani@intel.com>
2024-06-03 11:39:55 -0700

    remove absolute path

--- fuzzers/cva6-vcs-fuzzer/run.sh
+++ fuzzers/cva6-vcs-fuzzer/run.sh
@@ -1,1 +1,1 @@
-./simv +vcs+finish+20us -cm line+fsm+cond+tgl+branch -cm_dir Coverage.vdb  +permissive +elf_file=./testcase.elf ++./testcase.elf +debug_disable=1 +ntb_random_seed=1 -sv_lib /home/sdp/riscv/lib/libfesvr  +ntb_random_seed=28072
+./simv +vcs+finish+20us -cm line+fsm+cond+tgl+branch -cm_dir Coverage.vdb  +permissive +elf_file=./testcase.elf ++./testcase.elf +debug_disable=1 +ntb_random_seed=1 -sv_lib $RISCV/lib/libfesvr  +ntb_random_seed=28072
